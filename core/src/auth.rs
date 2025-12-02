1c8e1379822db240fa525ef2e8a446b959eb73c3
mohammadjavad948 <mjkmo123@gmail.com>
2025-12-03 01:11:56 +0330

    core auth

---
+++ core/src/auth.rs
@@ -0,0 +1,45 @@
+use axum::{extract::Request, http::{StatusCode}, middleware::Next, response::Response};
+
+pub trait AuthenticatableUser {
+    type Username;
+    type Password;
+    type Id;
+
+    fn get_username(&self) -> Self::Username;
+    fn get_password(&self) -> Self::Password;
+    fn get_id(&self) -> Self::Id;
+}
+
+pub trait Authenticator<T>
+    where T: AuthenticatableUser 
+{
+    fn attempt<R>(&self, username: T::Username, password: R) -> impl std::future::Future<Output = anyhow::Result<T>> + Send;
+    fn generate_token<Q>(&self, user: T) -> impl std::future::Future<Output = anyhow::Result<Q>> + Send;
+
+    fn verify_header_name(&self) -> &'static str;
+    fn verify(&self, token: &str) -> impl std::future::Future<Output = anyhow::Result<T>> + Send;
+}
+
+
+pub async fn auth_middleware<T, R>(mut request: Request, next: Next) -> Result<Response, StatusCode>
+    where T: Authenticator<R> + Send + Sync + 'static,
+          R: AuthenticatableUser + Send + Sync + Clone + 'static
+{
+    let Some(authenticator) = request.extensions().get::<T>() else {
+        tracing::error!("no Authenticator Extension available");
+
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    };
+
+    let Some(header) = request.headers().get(authenticator.verify_header_name()) else {
+        return Err(StatusCode::UNAUTHORIZED);
+    };
+
+    let header = header.to_str().map_err(|_| StatusCode::UNAUTHORIZED)?;
+
+    let user = authenticator.verify(header).await.map_err(|_| StatusCode::UNAUTHORIZED)?;
+
+    request.extensions_mut().insert(user);
+
+    Ok(next.run(request).await)
+}
