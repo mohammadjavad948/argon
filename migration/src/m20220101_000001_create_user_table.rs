9ac9eda7cd35419b1ffd82a6b14f572de4430586
mohammadjavad948 <mjkmo123@gmail.com>
2025-12-02 08:59:32 +0330

    docker compose and basic user table

--- migration/src/m20220101_000001_create_user_table.rs
+++ migration/src/m20220101_000001_create_user_table.rs
@@ -6,28 +6,24 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table("post")
+                    .table("user")
                     .if_not_exists()
                     .col(pk_auto("id"))
-                    .col(string("title"))
-                    .col(string("text"))
+                    .col(string("name").not_null())
+                    .col(string("username").unique_key().not_null())
+                    .col(string("password").not_null())
+                    .col(timestamp("created_at").default(Expr::current_timestamp()).not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table("post").to_owned())
+            .drop_table(Table::drop().table("user").to_owned())
             .await
     }
 }
