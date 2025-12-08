595185dcdff8cc97c366d06c954a2525b81b1c9d
mohammadjavad948 <mjkmo123@gmail.com>
2025-12-08 16:52:48 +0330

    fix

--- src/app/controller/mod.rs
+++ src/app/controller/mod.rs
@@ -4,7 +4,7 @@ pub struct TestController;
 impl TestController {
     #[argon_macros::get("/hello/{id}")]
     #[argon_macros::utoipa_response(response = crate::app::response::BasicResponse)]
-    pub async fn index(axum::extract::Path(id): axum::extract::Path<u64>,) -> () {
-        ()
+    pub async fn index(axum::extract::Path(id): axum::extract::Path<u64>) -> Result<crate::app::response::BasicResponse, crate::app::response::BasicResponse> {
+        unimplemented!()
     }
 }
